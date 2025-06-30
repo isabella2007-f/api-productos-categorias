d5e57856ca61727cc7a8043669281dbce925cc65
isabela <isafernandez060@gmail.com>
2025-06-29 23:07:44 -0500

    Descripción de los cambios realizados

--- api/views.py
+++ api/views.py
@@ -1,11 +1,16 @@
 from rest_framework import viewsets
 from .models import Categoria, Producto
 from .serializers import CategoriaSerializer, ProductoSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 class CategoriaViewSet(viewsets.ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
-
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"mensaje": "Categoría eliminada correctamente"}, status=status.HTTP_200_OK)
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
@@ -15,3 +20,8 @@ class ProductoViewSet(viewsets.ModelViewSet):
         if categoria_id:
             return Producto.objects.filter(categoria_id=categoria_id)
         return super().get_queryset()
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"mensaje": "Producto eliminado correctamente"}, status=status.HTTP_200_OK)
